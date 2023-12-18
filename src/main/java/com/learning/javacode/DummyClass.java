65cbfbb3e0eec82f2cd6124178e942ab8783a6fb
Andrei Ursu <ursuandrei42@gmail.com>
2023-12-18 16:18:43 +0200

    builtin datatypes

---
+++ src/main/java/com/learning/javacode/DummyClass.java
@@ -0,0 +1,9 @@
+package com.learning.javacode;
+
+public class DummyClass {
+
+    public String isVacationTime(Boolean onVacation) {
+        return onVacation ? "I'm on vacation!" : "I'm working";
+    }
+
+}
