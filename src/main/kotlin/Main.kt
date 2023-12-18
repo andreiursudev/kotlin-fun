fe95652e39efc88e2f426996dbf78f383ec91ca5
Andrei Ursu <ursuandrei42@gmail.com>
2023-12-18 12:37:33 +0200

    variable declaration

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+
+    //val = final
+    val number1: Int
+    val number2 = 25
+    val number3: Int = 25
+    val number4: Short = 25
+    //var = mutable
+    var number5: Int
+    number5 = 10
+
+    val employee1 = Employee("Lynn Jones", 500)
+    employee1.name = "Lynn Smith"
+
+    //employee1 = Employee("Tim Watson", 100) cannot assign a new instance to employee1 reference variable
+
+
+}
+
+class Employee(var name: String, val id: Int) {
+
 }
