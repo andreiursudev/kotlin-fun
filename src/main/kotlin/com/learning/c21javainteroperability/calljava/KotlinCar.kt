fb824273a89c869aa6feb6caa7cb1bc70aae2bdc
Andrei Ursu <ursuandrei42@gmail.com>
2023-12-21 12:01:27 +0200

    java interoperability part2

--- src/main/kotlin/com/learning/c21javainteroperability/calljava/KotlinCar.kt
+++ src/main/kotlin/com/learning/c21javainteroperability/calljava/KotlinCar.kt
@@ -2,10 +2,29 @@
 
 package com.learning.c21javainteroperability.calljava
 
+import java.io.IOException
+
 fun topLevel() = println("I'm in the Car file!")
 
 fun main(args: Array<String>) {
     "Print this".print()
+    KotlinCar.carComp()
+}
+
+//Only ine version of the function is generated from Kotlin to Java, the one with all parameters
+@JvmOverloads
+fun defaultArgs(str: String, num: Int = 25) {
+
+}
+
+@Throws(IOException::class)//We need to annotate the exception since Kotlin only uses runtime exceptions
+fun doIO() {
+    throw IOException()
+}
+
+object SingletonObj {
+    @JvmStatic
+    fun doSomething() = println("I'm doing something in the singleton object")
 }
 
 class KotlinCar(val color: String, @JvmField val model: String, val year: Int) {
@@ -14,6 +33,19 @@ class KotlinCar(val color: String, @JvmField val model: String, val year: Int) {
 //        private set(value) {
 //            field = "always green"
 //        }
+
+    companion object {
+        const val constant = 25
+        @JvmField
+        val isAuto = false
+        @JvmStatic
+        fun carComp() = println("I'm in Car's companion object")
+    }
+
+    fun printMe(text: String) {
+        println("I don't expect a null value")
+    }
+
 
 }
 
--- src/main/kotlin/com/learning/c21javainteroperability/javacode/Main.java
+++ src/main/kotlin/com/learning/c21javainteroperability/javacode/Main.java
@@ -2,19 +2,37 @@ package com.learning.c21javainteroperability.javacode;
 
 
 import com.learning.c21javainteroperability.calljava.KotlinCar;
+import com.learning.c21javainteroperability.calljava.SingletonObj;
 import com.learning.c21javainteroperability.calljava.StaticCar;
+
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
-		//KotlinCarKt.topLevel();
-	    StaticCar.topLevel();
+        //KotlinCarKt.topLevel();
+        StaticCar.topLevel();
 
-	    StaticCar.print("print this Java string");
+        StaticCar.print("print this Java string");
 
-		KotlinCar car = new KotlinCar("blue", "BMW", 2011);
-	    System.out.println(car.model);
-	    System.out.println(car.getColor());
+        KotlinCar car = new KotlinCar("blue", "BMW", 2011);
+        System.out.println(car.model);
 
+        System.out.println(car.getColor());
+        KotlinCar.carComp();
+        System.out.println("isAuto = " + KotlinCar.constant);
+
+        SingletonObj.doSomething();
+
+        //Throws IllegalArgumentException because Kotlin String is non-nullable
+        //car.printMe(null);
+
+        try {
+            StaticCar.doIO();
+        } catch (IOException e) {
+            System.out.println("IOException!");
+        }
+
+        StaticCar.defaultArgs("The Number is: ");
     }
 }
