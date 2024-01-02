643de7a4f5edb63c1367160e0d88d4ee70ed7f7e
Andrei Ursu <ursuandrei42@gmail.com>
2024-01-02 16:38:11 +0200

    coroutine scope

---
+++ src/main/kotlin/com/learning/c27coroutinescope/CoroutineNamePassingFromParentToChild.kt
@@ -0,0 +1,22 @@
+package com.learning.c27coroutinescope
+
+import kotlinx.coroutines.*
+
+suspend fun longTask() = coroutineScope {
+    launch {
+        delay(1000)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 1")
+    }
+    launch {
+        delay(2000)
+        val name = coroutineContext[CoroutineName]?.name
+        println("[$name] Finished task 2")
+    }
+}
+
+fun main() = runBlocking(CoroutineName("Parent")) {
+    println("Before")
+    longTask()
+    println("After")
+}
